72c3fbf3aec18f97d01e30c7a1919d8cae0ba084
AsanHo <gorma00@naver.com>
2025-10-20 17:01:39 +0900

    이미지 처리 기초

--- 02_Bloom/Example.h
+++ 02_Bloom/Example.h
@@ -80,12 +80,12 @@ public:
 		//	image.pixels[i].v[2] *= 100.0f;
 		//}
 		// 밝기 올리기v2
-		for (int i = 0; i < image.height * image.width; i++) // y
-		{
-			image.pixels[i].v[0] = std::clamp(image.pixels[i].v[0] *100.0f,0.0f,1.0f); // 이렇게 값이 커지면 오버플로우등이 날수있어 clamp를 써주어야함.
-			image.pixels[i].v[1] = std::clamp(image.pixels[i].v[1] *100.0f,0.0f,1.0f);
-			image.pixels[i].v[2] = std::clamp(image.pixels[i].v[2] *100.0f,0.0f,1.0f);
-		}
+		//for (int i = 0; i < image.height * image.width; i++) // y
+		//{
+		//	image.pixels[i].v[0] = std::clamp(image.pixels[i].v[0] *100.0f,0.0f,1.0f); // 이렇게 값이 커지면 오버플로우등이 날수있어 clamp를 써주어야함.
+		//	image.pixels[i].v[1] = std::clamp(image.pixels[i].v[1] *100.0f,0.0f,1.0f);
+		//	image.pixels[i].v[2] = std::clamp(image.pixels[i].v[2] *100.0f,0.0f,1.0f);
+		//}
 		/*
 		* 여기서 사용하는 방법은 이해하기 더 쉬운 Separable convolution 입니다.
 		*/
@@ -317,8 +317,29 @@ public:
 		pixels[1 + canvasWidth * 0] = Vec4{ 1.0f, 1.0f, 0.0f, 1.0f };
 		*/
 
-		//image.Blur(7);
+		
 
+		for (int j = 0; j < image.height; j++) // y
+		{
+			for (int i = 0; i < image.width; i++) // x
+			{
+				Vec4& pixel = image.GetPixel(i, j);
+				if (i < image.width / 2) {
+					pixel.v[0] = pixel.v[0] * 0.9; // R
+					pixel.v[1] = pixel.v[1] * 0.9; // G
+					pixel.v[2] = pixel.v[2] * 0.9; // B
+
+				}
+				else {
+					pixel.v[0] = pixel.v[0] * 1.1; // R
+					pixel.v[1] = pixel.v[1] * 1.1; // G
+					pixel.v[2] = pixel.v[2] * 1.1; // B
+				}
+				// pixel.v[3] = A 값은 그대로 둠
+			}
+		}
+		
+		//image.Blur(7);
 		//image.GaussianBlur5();
 
 		// 이미지의 내용을 GPU 메모리로 복사
